78971e82592ecee2bb4e149a0c139aa9601e5811
cr3559 <cr3559@cs.ship.edu>
2018-04-30 16:17:36 -0400

    "Added methods to build, drop, and populate the tables."

--- assignmentTwo/src/assignmentTwo/JDBC.java
+++ assignmentTwo/src/assignmentTwo/JDBC.java
@@ -618,6 +618,203 @@ public class JDBC
 			}
 		}
 	}
+	
+	/*
+	 * Creates an ArrayList of SQL strings, is passed to
+	 * buildDataTables();
+	 * @return - the arraylist filled with sql create strings
+	 */
+	public ArrayList<String> buildTableList() throws SQLException
+	{
+		checkConnection();
+		ArrayList<String> tables = new ArrayList<String>();
+		tables.add("CREATE TABLE MODERATOR(Login VARCHAR(50) NOT NULL, Email VARCHAR(50) NOT NULL CHECK(LEN(Email) >= 7),PRIMARY KEY (Login));");
+		tables.add("CREATE TABLE SPECIAL_MODERATOR_ABILITIES (AbilityID INT NOT NULL CHECK (AbilityID >= 0 AND AbilityID <= 3 ), PRIMARY KEY (AbilityID));");
+		tables.add("CREATE TABLE MANAGER(Login VARCHAR(50) NOT NULL, Email VARCHAR(50) NOT NULL CHECK(LEN(Email) >= 7),PRIMARY KEY (Login));");
+		tables.add("CREATE TABLE SPECIAL_MANAGER_ABILITIES (AbilityID INT NOT NULL CHECK (AbilityID >= 0 AND AbilityID <= 2),PRIMARY KEY (AbilityID));");
+		tables.add("CREATE TABLE MODERATOR_ABILITIES (Login VARCHAR(50) NOT NULL, AbilityID INT NOT NULL CHECK (AbilityID >= 0 AND AbilityID <= 3), PRIMARY KEY(Login, AbilityID), FOREIGN KEY(Login) REFERENCES MODERATOR(Login), FOREIGN KEY(AbilityID) REFERENCES SPECIAL_MODERATOR_ABILITIES(AbilityID) ON DELETE CASCADE ON UPDATE CASCADE);");
+		tables.add("CREATE TABLE MANAGER_ABILITIES (Login VARCHAR(50) NOT NULL, AbilityID INT NOT NULL CHECK(AbilityID >= 0 AND AbilityID <= 2 ),PRIMARY KEY(Login, AbilityID),FOREIGN KEY(Login) REFERENCES MANAGER (Login),FOREIGN KEY(AbilityID) REFERENCES SPECIAL_MANAGER_ABILITIES(AbilityID) ON DELETE CASCADE ON UPDATE CASCADE);");
+		tables.add("CREATE TABLE PLAYER(Login VARCHAR(50) NOT NULL, PlayerID INT NOT NULL UNIQUE CHECK( 0 <=  PlayerID <= 50000000), Password VARCHAR(25) NOT NULL, Email VARCHAR(50) NOT NULL CHECK(LEN(Email) >= 7),PRIMARY KEY(Login, PlayerID));");
+		tables.add("CREATE TABLE PLAYS_AS(PlayerID INT NOT NULL CHECK( 0<=  PlayerID <= 50000000),CharacterName VARCHAR(25) NOT NULL UNIQUE CHECK(LEN(CharacterName)>= 8), Email VARCHAR(50) NOT NULL CHECK(LEN(Email) >=7),PRIMARY KEY(PlayerID),FOREIGN KEY(PlayerID) REFERENCES PLAYER (PlayerID) ON DELETE CASCADE ON UPDATE CASCADE);");
+		tables.add("CREATE TABLE PLAYABLE_CHARACTERS(PlayerID INT NOT NULL CHECK( 0<=  PlayerID <= 50000000),CharacterName VARCHAR(25) NOT NULL UNIQUE CHECK(LEN(CharacterName) >= 8),PRIMARY KEY(PlayerID),FOREIGN KEY( PlayerID) REFERENCES PLAYER(PlayerID) ON DELETE CASCADE ON UPDATE CASCADE);");
+		tables.add("CREATE TABLE HEROS(Name VARCHAR(50) NOT NULL CHECK(LEN(Name) >= 7),Stamina INT NOT NULL CHECK(0 < Stamina <= 100), MaxStrength INT NOT NULL CHECK(0 < MaxStrength <= 100),Strength INT NOT NULL CHECK(0 < Strength <= 100), MaxHP INT NOT NULL CHECK(0 < MaxHP <= 100), PRIMARY KEY (Name),FOREIGN KEY (Name) REFERENCES PLAYS_AS (CharacterName),FOREIGN KEY (Name) REFERENCES PLAYABLE_CHARACTERS(CharacterName) ON DELETE CASCADE ON UPDATE CASCADE);");
+		tables.add("CREATE TABLE LOCATION(LocationID INT NOT NULL CHECK (100000001 <= LocationID <=150000000),Size INT NOT NULL CHECK (10 <= Size <= 1000),Type INT NOT NULL CHECK ( 0 <= Type <=2), PRIMARY KEY (LocationID));");
+		tables.add("CREATE TABLE CREATURE (CreatureID INT NOT NULL CHECK(50000001 <= CreatureID <= 100000000),DamageProtection INT NOT NULL CHECK(1 <= DamageProtection <= 100),CurrentHP INT NOT NULL CHECK(1 <= CurrentHP <= 100), MaxHP INT NOT NULL CHECK(1 <= MaxHP <= 100), MaxStrength INT NOT NULL CHECK(1 <= MaxStrength <= 100),Strength INT NOT NULL CHECK(1 <= Strength <= 100),Stamina INT NOT NULL CHECK(1 <= Stamina <= 100),PRIMARY KEY (CreatureID));");
+		tables.add("CREATE TABLE LIKES( CreatureID INT NOT NULL CHECK(50000001<= CreatureID <=100000000), PlayerID INT NOT NULL CHECK( 0<=  PlayerID <= 50000000), PRIMARY KEY(CreatureID, PlayerID), FOREIGN KEY (CreatureID) References CREATURE(CreatureID), FOREIGN KEY (PlayerID) REFERENCES PLAYER(PlayerID) ON DELETE CASCADE ON UPDATE CASCADE);");
+		tables.add("CREATE TABLE HATES(CreatureID INT NOT NULL CHECK(50000001<= CreatureID <=100000000),PlayerID INT NOT NULL CHECK( 0<=  PlayerID <= 50000000),PRIMARY KEY(CreatureID, PlayerID),FOREIGN KEY (CreatureID) References CREATURE(CreatureID),FOREIGN KEY (PlayerID) REFERENCES PLAYER(PlayerID) ON DELETE CASCADE ON UPDATE CASCADE);");
+		tables.add("CREATE TABLE NEUTRAL_TO (CreatureID INT NOT NULL CHECK(50000001<= CreatureID <= 100000000),PlayerID INT NOT NULL CHECK( 0<=  PlayerID <= 50000000),PRIMARY KEY(CreatureID, PlayerID), FOREIGN KEY (CreatureID) REFERENCES CREATURE(CreatureID),FOREIGN KEY (PlayerID) REFERENCES PLAYER(PlayerID) ON DELETE CASCADE ON UPDATE CASCADE);");
+		tables.add("CREATE TABLE LIKED_BY (CharacterName VARCHAR(50) NOT NULL CHECK(LEN(Name) >= 7),CreatureID INT NOT NULL CHECK(50000001<= CreatureID <= 100000000),PRIMARY KEY(CharacterName, CreatureID),FOREIGN KEY(CharacterName) REFERENCES HEROS(Name),FOREIGN KEY(CreatureID) REFERENCES CREATURE(CreatureID) ON DELETE CASCADE ON UPDATE CASCADE);");
+		tables.add("CREATE TABLE HATED_BY (CharacterName VARCHAR(50) NOT NULL CHECK(LEN(Name) >= 7),CreatureID INT NOT NULL CHECK(50000001<= CreatureID <= 100000000),PRIMARY KEY(CharacterName, CreatureID),FOREIGN KEY(CharacterName) REFERENCES HEROS(Name),FOREIGN KEY(CreatureID) REFERENCES CREATURE(CreatureID) ON DELETE CASCADE ON UPDATE CASCADE);");
+		tables.add("CREATE TABLE ACCESSIBLE_AREAS (CreatureID INT NOT NULL CHECK(50000001<= CreatureID <= 100000000),LocationID INT NOT NULL CHECK (100000001 <= LocationID <= 150000000),PRIMARY KEY(CreatureID, LocationID),FOREIGN KEY( CreatureID) REFERENCES CREATURE(CreatureID),FOREIGN KEY( LocationID) REFERENCES LOCATION(LocationID) ON DELETE CASCADE ON UPDATE CASCADE);");
+		tables.add("CREATE TABLE ABILITY(AbilityID INT NOT NULL CHECK(200000001 <= AbilityID <= 250000000),TargetedStat INT NOT NULL CHECK (0 <= TargetedStat <= 3),AffectAmmount INT NOT NULL CHECK (1 <= AffectAmmount <= 100),ExecutionTime INT NOT NULL CHECK ( 1 <= ExecutionTime <= 5), PRIMARY KEY(AbilityID));");
+		tables.add("CREATE TABLE HAS_ABILITY(CreatureID INT NOT NULL CHECK(50000001<= CreatureID <= 100000000),AbilityID INT NOT NULL CHECK(200000001 <= AbilityID <= 250000000), PRIMARY KEY(CreatureID, AbilityID),FOREIGN KEY(CreatureID) REFERENCES CREATURE(CreatureID),FOREIGN KEY(AbilityID) REFERENCES ABILITY(AbilityID) ON DELETE CASCADE ON UPDATE CASCADE);");
+		tables.add("CREATE TABLE GENERIC_ITEM (ItemID INT NOT NULL CHECK(150000001 <= ItemID <= 200000000),Weight INT NOT NULL CHECK (1 <= Weight <= 25),Volume INT NOT NULL CHECK (1 <= Volume <= 25),Description VARCHAR(100) NOT NULL CHECK(LEN(Description) >= 5),3DModel VARCHAR(30) NOT NULL CHECK(LEN(3DModel) >= 5),PRIMARY KEY(ItemID));");
+		tables.add("CREATE TABLE CONTAINER (ItemID INT NOT NULL CHECK(150000001 <= ItemID <= 200000000),WeightLimit INT NOT NULL CHECK (1 <= WeightLimit <= 200),VolumeLimit INT NOT NULL CHECK (1 <= VolumeLimit <= 200),PRIMARY KEY(ItemID),FOREIGN KEY(ItemID) REFERENCES GENERIC_ITEM(ItemID) ON DELETE CASCADE ON UPDATE CASCADE);");
+		tables.add("CREATE TABLE WEAPON(ItemID INT NOT NULL CHECK(150000001 <= ItemID <= 200000000),Ability INT NOT NULL CHECK(200000001 <= Ability <= 200000000),StoringLocation INT NOT NULL CHECK (0 <= StoringLocation <=2),PRIMARY KEY(ItemID, Ability),FOREIGN KEY(ItemID) REFERENCES GENERIC_ITEM(ItemID),FOREIGN KEY(Ability)REFERENCES ABILITY(AbilityID) ON DELETE CASCADE ON UPDATE CASCADE);");
+		tables.add("CREATE TABLE ARMOR (ItemID INT NOT NULL CHECK(150000001 <= ItemID <= 200000000),WearLocation INT NOT NULL CHECK(0 <= WearLocation <=3),DamageProtection INT NOT NULL CHECK (1 <= DamageProtection <= 99),PRIMARY KEY (ItemID),FOREIGN KEY(ItemID) REFERENCES GENERIC_ITEM(ItemID) ON DELETE CASCADE ON UPDATE CASCADE);");
+		tables.add("CREATE TABLE CONTAINS(LocationID INT NOT NULL CHECK (100000001 <= LocationID <=150000000),PlayerID INT NOT NULL CHECK( 0<=  PlayerID <= 50000000),ItemID INT NOT NULL CHECK(150000001 <= ItemID <= 200000000),CreatureID INT NOT NULL CHECK(50000001<= CreatureID <= 100000000),PRIMARY KEY(LocationID, PlayerID, ItemID, CreatureID),FOREIGN KEY (LocationID) REFERENCES LOCATION(LocationID),FOREIGN KEY (PlayerID) REFERENCES PLAYER(PlayerID),FOREIGN KEY (ItemID) REFERENCES GENERIC_ITEM (ItemID),FOREIGN KEY (CreatureID) REFERENCES CREATURE(CreatureID));");
+		return tables;
+	}
+	
+	/*
+	 * Builds tables in the database
+	 */
+	public void buildDataTables(ArrayList<String> tables) throws SQLException
+	{
+		for(int i = 0; i < tables.size(); i++)
+		{
+			PreparedStatement statement2 = m_dbConn.prepareStatement(tables.get(i));
+			System.out.println(statement2.toString());
+			m_dbConn.setAutoCommit(false);
+			statement2.execute();
+			m_dbConn.commit();
+		}
+	}
+	
+	/*
+	 * Drops tables in the database. Used for testing purposes
+	 */
+	public void dropDataTables() throws SQLException
+	{
+		checkConnection();
+		ArrayList<String> tableNames = new ArrayList<String>();
+		tableNames.add("MODERATOR");
+		tableNames.add("SPECIAL_MODERATOR_ABILITIES");
+		tableNames.add("MANAGER");
+		tableNames.add("SPECIAL_MANAGER_ABILITIES");
+		tableNames.add("MODERATOR_ABILITIES");
+		tableNames.add("MANAGER_ABILITIES");
+		tableNames.add("PLAYER");
+		tableNames.add("PLAYS_AS");
+		tableNames.add("PLAYABLE_CHARACTERS");
+		tableNames.add("HEROS");
+		tableNames.add("LOCATION");
+		tableNames.add("CREATURE");
+		tableNames.add("LIKES");
+		tableNames.add("HATES");
+		tableNames.add("NEUTRAL_TO");
+		tableNames.add("LIKED_BY");
+		tableNames.add("HATED_BY");
+		tableNames.add("ACCESSIBLE_AREAS");
+		tableNames.add("ABILITY");
+		tableNames.add("HAS_ABILITY");
+		tableNames.add("GENERIC_ITEM");
+		tableNames.add("CONTAINER");
+		tableNames.add("WEAPON");
+		tableNames.add("ARMOR");
+		tableNames.add("CONTAINS");
+		
+		String drop = "DROP TABLE";
+		for (int i = tableNames.size()-1; i > -1; i--)
+		{
+			try {
+				Statement statement = m_dbConn.createStatement();
+				statement.execute(drop + " " + tableNames.get(i));
+				}
+			catch(Exception e)
+			{
+				
+			}
+		}
+	}
+	/*
+	 * Populates tables in the database
+	 */
+	public void insertData() throws SQLException
+	{
+		checkConnection();
+		ArrayList<String> insertStatements = new ArrayList<String>();
+		insertStatements.add("INSERT INTO MODERATOR VALUES (\"SwagDaddy69\", \"SwagDaddy69@gmail.com\");");
+		insertStatements.add("INSERT INTO MODERATOR VALUES (\"BigPapiMcgee\", \"BigPapiMcgee@gmail.com\");");
+		insertStatements.add("INSERT INTO MODERATOR VALUES (\"TheZucc\", \"Robot@gmail.com\");");
+		insertStatements.add("INSERT INTO SPECIAL_MODERATOR_ABILITIES VALUES(0);");
+		insertStatements.add("INSERT INTO SPECIAL_MODERATOR_ABILITIES VALUES(1);");
+		insertStatements.add("INSERT INTO SPECIAL_MODERATOR_ABILITIES VALUES(2);");
+		insertStatements.add("INSERT INTO MANAGER VALUES(\"Mike Oxlong\", \"mikeoxlong@gmail.com\");");
+		insertStatements.add("INSERT INTO MANAGER VALUES(\"Phil McCrackin\", \"philmccrackin@gmail.com\");");
+		insertStatements.add("INSERT INTO MANAGER VALUES(\"Nick Cage\", \"notthebees@gmail.com\");");
+		insertStatements.add("INSERT INTO SPECIAL_MANAGER_ABILITIES VALUES(0);");
+		insertStatements.add("INSERT INTO SPECIAL_MANAGER_ABILITIES VALUES(1);");
+		insertStatements.add("INSERT INTO SPECIAL_MANAGER_ABILITIES VALUES(2);");
+		insertStatements.add("INSERT INTO MODERATOR_ABILITIES VALUES(\"SwagDaddy69\", 0 );");
+		insertStatements.add("INSERT INTO MODERATOR_ABILITIES VALUES(\"BigPapiMcgee\", 1 );");
+		insertStatements.add("INSERT INTO MODERATOR_ABILITIES VALUES(\"TheZucc\", 2 );");
+		insertStatements.add("INSERT INTO MANAGER_ABILITIES VALUES(\"Mike Oxlong\",0 );");
+		insertStatements.add("INSERT INTO MANAGER_ABILITIES VALUES(\"Phil McCrackin\",1 );");
+		insertStatements.add("INSERT INTO MANAGER_ABILITIES VALUES(\"Nick Cage\", 2 );");
+		insertStatements.add("INSERT INTO PLAYER VALUES(\"BadAss\", 1, \"secretpassword\", \"Badassmfr@gmail.com\");");
+		insertStatements.add("INSERT INTO PLAYER VALUES(\"CreepyOldMan\", 2, \"imcreepy\", \"serialkiller@gmail.com\");");
+		insertStatements.add("INSERT INTO PLAYER VALUES(\"HungryTeen\", 3, \"gimmedatfood\", \"cerealkiller@gmail.com\");");
+		insertStatements.add("INSERT INTO PLAYS_AS VALUES(1, \"BAMF\", \"crunchatizeme@gmail.com\");");
+		insertStatements.add("INSERT INTO PLAYS_AS VALUES(2, \"FreeCandyMan\", \"FreeCandy@gmail.com\");");
+		insertStatements.add("INSERT INTO PLAYS_AS VALUES(3, \"IEatFreeCandy\", \"FreeCandyPlz@gmail.com\");");
+		insertStatements.add("INSERT INTO PLAYABLE_CHARACTERS VALUES(1, \"BAMF\");");
+		insertStatements.add("INSERT INTO PLAYABLE_CHARACTERS VALUES(2, \"FreeCandyMan\");");
+		insertStatements.add("INSERT INTO PLAYABLE_CHARACTERS VALUES(3, \"IEatFreeCandy\");");
+		insertStatements.add("INSERT INTO HEROS VALUES(\"BAMF\", 100, 100, 100, 100);");
+		insertStatements.add("INSERT INTO HEROS VALUES(\"FreeCandyMan\", 100, 100, 100, 100);");
+		insertStatements.add("INSERT INTO HEROS VALUES(\"IEatFreeCandy\", 100, 100, 100, 100);");
+		insertStatements.add("INSERT INTO LOCATION VALUES(100000005, 100, 0);");
+		insertStatements.add("INSERT INTO LOCATION VALUES(100000006, 100, 1);");
+		insertStatements.add("INSERT INTO LOCATION VALUES(100000007, 100, 2);");
+		insertStatements.add("INSERT INTO CREATURE VALUES(50000005, 50, 100, 100, 100, 100, 100);");
+		insertStatements.add("INSERT INTO CREATURE VALUES(50000006, 60, 100, 100, 100, 100, 100);");
+		insertStatements.add("INSERT INTO CREATURE VALUES(50000007, 70, 100, 100, 100, 100, 100);");
+		insertStatements.add("INSERT INTO LIKES VALUES(50000005, 1);");
+		insertStatements.add("INSERT INTO LIKES VALUES(50000006, 1);");
+		insertStatements.add("INSERT INTO LIKES VALUES(50000007, 1);");
+		insertStatements.add("INSERT INTO HATES VALUES(50000005, 2);");
+		insertStatements.add("INSERT INTO HATES VALUES(50000006, 2);");
+		insertStatements.add("INSERT INTO HATES VALUES(50000007, 2);");
+		insertStatements.add("INSERT INTO NEUTRAL_TO VALUES(50000005, 3);");
+		insertStatements.add("INSERT INTO NEUTRAL_TO VALUES(50000006, 3);");
+		insertStatements.add("INSERT INTO NEUTRAL_TO VALUES(50000007, 3);");
+		insertStatements.add("INSERT INTO LIKED_BY VALUES(\"BAMF\", 50000005);");
+		insertStatements.add("INSERT INTO LIKED_BY VALUES(\"BAMF\", 50000006);");
+		insertStatements.add("INSERT INTO LIKED_BY VALUES(\"BAMF\", 50000007);");
+		insertStatements.add("INSERT INTO HATED_BY VALUES(\"FreeCandyMan\", 50000005);");
+		insertStatements.add("INSERT INTO HATED_BY VALUES(\"FreeCandyMan\", 50000006);");
+		insertStatements.add("INSERT INTO HATED_BY VALUES(\"FreeCandyMan\", 50000007);");
+		insertStatements.add("INSERT INTO ACCESSIBLE_AREAS VALUES(50000005, 100000005);");
+		insertStatements.add("INSERT INTO ACCESSIBLE_AREAS VALUES(50000006, 100000006);");
+		insertStatements.add("INSERT INTO ACCESSIBLE_AREAS VALUES(50000007, 100000007);");
+		insertStatements.add("INSERT INTO ABILITY VALUES(250000000, 0, 50, 1);");
+		insertStatements.add("INSERT INTO ABILITY VALUES(250000001, 1, 50, 2);");
+		insertStatements.add("INSERT INTO ABILITY VALUES(250000002, 2, 50, 3);");
+		insertStatements.add("INSERT INTO HAS_ABILITY VALUES(50000005, 250000000);");
+		insertStatements.add("INSERT INTO HAS_ABILITY VALUES(50000006, 250000001);");
+		insertStatements.add("INSERT INTO HAS_ABILITY VALUES(50000007, 250000002);");
+		insertStatements.add("INSERT INTO GENERIC_ITEM VALUES(150000001, 20, 20, \"generic1\", \"jawn.dxf\");");
+		insertStatements.add("INSERT INTO GENERIC_ITEM VALUES(150000002, 20, 20, \"generic2\", \"poo.dxf\");");
+		insertStatements.add("INSERT INTO GENERIC_ITEM VALUES(150000003, 20, 20, \"generic3\", \"shoe.dxf\");");
+		insertStatements.add("INSERT INTO GENERIC_ITEM VALUES(150000004, 20, 20, \"generic4\", \"sock.dxf\");");
+		insertStatements.add("INSERT INTO GENERIC_ITEM VALUES(150000005, 20, 20, \"generic5\", \"lemon.dxf\");");
+		insertStatements.add("INSERT INTO GENERIC_ITEM VALUES(150000006, 20, 20, \"generic6\", \"beer.dxf\");");
+		insertStatements.add("INSERT INTO GENERIC_ITEM VALUES(150000007, 20, 20, \"generic7\", \"dillydilly.dxf\");");
+		insertStatements.add("INSERT INTO GENERIC_ITEM VALUES(150000008, 20, 20, \"generic8\", \"coffee.dxf\");");
+		insertStatements.add("INSERT INTO GENERIC_ITEM VALUES(150000009, 20, 20, \"generic9\", \"balls.dxf\");");
+		insertStatements.add("INSERT INTO CONTAINER VALUES(150000001, 150,150);");
+		insertStatements.add("INSERT INTO CONTAINER VALUES(150000002, 150,150);");
+		insertStatements.add("INSERT INTO CONTAINER VALUES(150000003, 150,150);");
+		insertStatements.add("INSERT INTO WEAPON VALUES(150000004, 250000000, 1);");
+		insertStatements.add("INSERT INTO WEAPON VALUES(150000005, 250000001, 2);");
+		insertStatements.add("INSERT INTO WEAPON VALUES(150000006, 250000002, 3);");
+		insertStatements.add("INSERT INTO ARMOR VALUES(150000007, 1, 50);");
+		insertStatements.add("INSERT INTO ARMOR VALUES(150000008, 2, 50);");
+		insertStatements.add("INSERT INTO ARMOR VALUES(150000009, 3, 50);");
+		insertStatements.add("INSERT INTO CONTAINS VALUES(100000005, 1, 150000004,50000005);");
+		insertStatements.add("INSERT INTO CONTAINS VALUES(100000006, 2, 150000005,50000006);");
+		insertStatements.add("INSERT INTO CONTAINS VALUES(100000007, 3, 150000006,50000007);");
+
+		for (int i =0; i < insertStatements.size(); i++)
+		{
+			PreparedStatement statement2 = m_dbConn.prepareStatement(insertStatements.get(i));
+			System.out.println(statement2.toString());
+			m_dbConn.setAutoCommit(false);
+			statement2.execute();
+			m_dbConn.commit();
+		}
+	}
 
 
 }
